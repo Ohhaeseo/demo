b144066d65739664a962f6d28283144dfc66cc42
Ohhaeseo <dhgotj@naver.com>
2024-10-03 15:52:17 +0900

    DAY5 추가구현 완료!

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -16,24 +16,32 @@
 // }
 
 // 이 클래스가 속한 패키지를 선언합니다.
-package com.example.demo;
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 // Spring Framework에서 제공하는 Controller 애노테이션을 임포트합니다.
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 // Spring의 Model 인터페이스를 임포트합니다. 이는 뷰에 데이터를 전달하는 데 사용됩니다.
 import org.springframework.ui.Model;
 // HTTP GET 요청을 특정 메서드에 매핑하기 위한 애노테이션을 임포트합니다.
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.service.TestService;
+
 
 // 이 클래스가 Spring MVC의 컨트롤러임을 나타냅니다.
 // 이 애노테이션은 Spring에게 이 클래스가 웹 요청을 처리할 수 있음을 알려줍니다.
 @Controller
 
+
+
 // DemoController 클래스를 정의합니다.
 public class DemoController {
-    
+
     // "/hello" URL로 들어오는 HTTP GET 요청을 이 메서드가 처리하도록 매핑합니다.
     @GetMapping("/hello")
     // hello 메서드를 정의합니다. Model 객체를 파라미터로 받습니다.
@@ -72,6 +80,26 @@ public class DemoController {
         model.addAttribute("para1", "001");
         model.addAttribute("para2", 002);
     return "thymeleaf_test1";
+    }
+
+    @Autowired
+    private TestService testService;
+    
+
+    @GetMapping("/testdb")
+    public String getAllTestDBs(Model model) {
+        TestDB test = testService.findByName("홍길동");
+        TestDB test1 = testService.findByName("아저씨");
+        TestDB test2 = testService.findByName("꾸러기");
+
+        model.addAttribute("data4", test);
+        model.addAttribute("data5", test1);
+        model.addAttribute("data6", test2);
+
+        System.out.println("데이터 출력 디버그 : " + test);
+        System.out.println("데이터 출력 디버그 : " + test1);
+        System.out.println("데이터 출력 디버그 : " + test2);
+        return "testdb";
     }
 
     
